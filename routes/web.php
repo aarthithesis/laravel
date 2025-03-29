e958082b1d02230c305deea9ceee184cd5169b1e
aarthithesis <162783395+aarthithesis@users.noreply.github.com>
2025-03-28 20:29:42 -0400

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,17 @@ use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 // Route to display the form
-Route::get('/form', [PageController::class, 'form']);
+Route::get('/form', [PageController::class, 'form'])->name('user');
 
 // Route to handle form submission
 Route::post('/submit', [PageController::class, 'submit']);
 
+Route::get('/{name}', function($name) {
+    return "Welcome $name <a href='" . route('user') . "'>register here</a>";
+});
+
+
+Route::get('/form', function() {
+    return view('form');
+})->name('user');
 ?>
